189a222a84438fa58798d27c5c90928a1bd06e46
tomasczerniawski <115027239+tomasczerniawski@users.noreply.github.com>
2022-10-11 19:09:09 +0300

    Update Cwiczenie1.C

--- Cwiczenie1.C
+++ Cwiczenie1.C
@@ -4,11 +4,32 @@
 #include <stdio.h>
 
 int main() {
-int a,x,y,c;
+int a,x,y;
  x=0;
  a=2;
    while(x<7){
-x=x+2;
+    x=x+2;
+    y=a*x*x;
+    
+   printf(" %d \n", y);
+  }
+  return 0;
+  }
+```
+# Zadanie2.C
+```
+#include <stdio.h>
+
+int main() {
+int a,x,y,n,h;
+ x=0;
+ a=2;
+   printf("wprowadz do jakiej zmiennej musi zkaczyc kod: ");
+    scanf("%d", &n);
+   printf("wprowadz krok: ");
+    scanf("%d", &h);
+  while(x<n){
+    x=x+h;
     y=a*x*x;
     
     printf(" %d \n", y);
