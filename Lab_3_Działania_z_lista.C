32905a3c6dd17b73431796464897a6025355effb
tomasczerniawski <115027239+tomasczerniawski@users.noreply.github.com>
2022-11-08 22:48:53 +0200

    Update Lab_3_Działania_z_lista.C

--- Lab_3_Działania_z_lista.C
+++ Lab_3_Działania_z_lista.C
@@ -1,1 +1,45 @@
+//Tomas Czerniawski. Student
+    // Lista jednokierunkowa niesortowana
+    // 2022-11-08
 
+    struct Item {
+        float value;
+        struct Item *next;
+        struct Item *prev;
+    };
+
+    float find(float value, Item *head)
+
+    Item locate(float value, Item *head) // pointer
+
+    float retrieve(Item *item) // wartosc
+
+    void insert(Item *item, float value, Item *head)
+
+    void remove(Item *item, Item *head)
+
+    int size(int *head)
+
+    Item findMax(Item *head)
+
+    Item findMin(Item *head)
+
+    int printList(Item *head)
+
+    int isEmpty(Item *head)
+
+    void next(Item *item, Item *head)
+
+    void previous(Item *item, Item *head)
+
+    void makeNull(Item *head)
+
+    #include <stdio.h>
+
+    void main(void) {
+        // realizacja
+
+      return;
+    }
+
+    // Funkcii
