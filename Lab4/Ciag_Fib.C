219502a522c2d56f6a16baee0c2607d8df14bd06
tomasczerniawski <115027239+tomasczerniawski@users.noreply.github.com>
2022-11-28 14:08:36 +0200

    Update and rename Lab4 to Ciag_Fib.C

---
+++ Lab4/Ciag_Fib.C
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int fib(int n){
+    
+    if (n==0) return 0;
+    if(n==1) return 1;
+    if(n>1) return fib(n-1)+fib(n-2);
+  
+}
+
+int main()
+  
+{
+int x;
+  
+    printf("Podaj element ciągu Fibonacciego do obliczenia:");
+  
+scanf("%d", &x);
+    
+    printf("Liczba = %d",x );
+    printf("ciągu Fibonacciego wynosi %d",fib(x));
+
+    return 0;
+}
