6314690bdad2ee588edc9b1c2e3c3a95933278ed
tomasczerniawski <115027239+tomasczerniawski@users.noreply.github.com>
2022-12-21 23:31:51 +0200

    Create mergeSort.h

---
+++ Lab_6/mergeSort.h
@@ -0,0 +1,107 @@
+//Ten kod niepracuje nic niewyswietla i niewychodzi mnie niejak poprawic
+
+#include <stdio.h>
+#include <errno.h>
+#include <stdio.h>
+#include <time.h>
+#define N 10000
+int arr[N];
+
+void merge(int *arr, int l, int m, int r)
+{ 
+ int i = 0, j, n = 0;
+  
+   
+  
+    FILE* fptr; 
+  
+    fptr = fopen("random.txt", "r"); 
+  
+    while (fscanf(fptr, "%d", &arr[i]) == 1) 
+    {
+      
+        n++; 
+  
+        i++; 
+    }
+    
+    clock_t t;
+
+    t = clock();
+     
+     //logika sortowania
+    
+   
+     
+  int k;
+  int n1 = m - l + 1;
+  int n2 = r - m;
+  int L[n1], R[n2];
+
+  for (i = 0; i < n1; i++)
+    L[i] = arr[l + i];
+  for (j = 0; j < n2; j++)
+    R[j] = arr[m + 1 + j];
+
+  i = 0;
+  j = 0;
+  k = l;
+
+  while (i < n1 && j < n2) {
+    if (L[i] <= R[j]) {
+      arr[k] = L[i];
+      i++;
+    } else {
+      arr[k] = R[j];
+      j++;
+    }
+    k++;
+  }
+
+  while (i < n1) {
+    arr[k] = L[i];
+    i++;
+    k++;
+  }
+
+  while (j < n2) {
+    arr[k] = R[j];
+    j++;
+    k++;
+  }
+
+    
+         
+   
+    
+    t = clock() - t;
+    double time_taken = ((double)t)/CLOCKS_PER_SEC;
+    
+    //printowanie tabeli
+    
+    
+
+	printf("\n sortowanie scalieniem trwalo %f seconds \n", time_taken);
+} 
+
+void mergeSort(int arr[], int l, int r) {
+  if (l < r) {
+    int m = l + (r - l) / 2;
+
+    mergeSort(arr, l, m);
+    mergeSort(arr, m + 1, r);
+
+    merge(arr, l, m, r);
+
+    
+  }
+}
+
+void printArray(int arr[], int n)
+{
+    int i;
+    for (i = 0; i < n; i++)
+      
+        printf("%d ", arr[i]);
+        printf("\n");
+}
