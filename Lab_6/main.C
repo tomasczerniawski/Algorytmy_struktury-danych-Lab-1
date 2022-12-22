128ae041a83e2cf068b0f380be911048a3ea9222
tomasczerniawski <115027239+tomasczerniawski@users.noreply.github.com>
2022-12-22 13:35:26 +0200

    Update main.C

--- Lab_6/main.C
+++ Lab_6/main.C
@@ -2,12 +2,25 @@
 
 #include "insertionSort.h"
 
+#include "selectionSort.h"
+
+#include "mergeSort.h"
+
+#include "quickSort.h"
+
+#include "GENEROWANIE.h"
+
 int main(void) {
 
-
-int i,n ,arr[1000];
+int i,n,count;
+//generacja();
    
-     // bubble_Sort();
-      insertion_Sort() ;
-      
+    // bubble_Sort();
+      //insertion_Sort() ;
+      //selectionSort();
+   // mergeSort(arr, 0, n - 1);
+   quick_Sort(arr,0,n-1);
+   
+   
+     
 }
