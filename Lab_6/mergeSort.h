2a83217ebe38991f9f67650de8f42fc23ddb2792
tomasczerniawski <115027239+tomasczerniawski@users.noreply.github.com>
2023-02-05 14:39:22 +0200

    Update mergeSort.h

--- Lab_6/mergeSort.h
+++ Lab_6/mergeSort.h
@@ -1,73 +1,65 @@
-//Ten kod niepracuje nic niewyswietla i niewychodzi mnie niejak poprawic
+
 
 #include <stdio.h>
 #include <errno.h>
-#include <stdio.h>
 
 #define N 10000
 int arr[N];
 
 void merge(int *arr, int l, int m, int r)
 {
-  int i = 0, j, k;
-  int n1 = m - l + 1;
-  int n2 = r - m;
-  int L[n1], R[n2];
+    int i = 0, j = 0, k = l;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+    int L[n1], R[n2];
 
-  for (i = 0; i < n1; i++)
-    L[i] = arr[l + i];
-  for (j = 0; j < n2; j++)
-    R[j] = arr[m + 1 + j];
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
 
-  i = 0;
-  j = 0;
-  k = l;
+    i = 0;
+    j = 0;
 
-  while (i < n1 && j < n2) {
-    if (L[i] <= R[j]) {
-      arr[k] = L[i];
-      i++;
-    } else {
-      arr[k] = R[j];
-      j++;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
     }
-    k++;
-  }
 
-  while (i < n1) {
-    arr[k] = L[i];
-    i++;
-    k++;
-  }
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
 
-  while (j < n2) {
-    arr[k] = R[j];
-    j++;
-    k++;
-  }
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
 }
 
-	
-} 
-
 void mergeSort(int arr[], int l, int r) {
-  if (l < r) {
-    int m = l + (r - l) / 2;
+    if (l < r) {
+        int m = l + (r - l) / 2;
 
-    mergeSort(arr, l, m);
-    mergeSort(arr, m + 1, r);
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
 
-    merge(arr, l, m, r);
-
-    
-  }
+        merge(arr, l, m, r);
+    }
 }
 
 void printArray(int arr[], int n)
 {
     int i;
     for (i = 0; i < n; i++)
-      
         printf("%d ", arr[i]);
-        printf("\n");
+    printf("\n");
 }
