e201cd23d0b665c79d1b008a86e287de6ec8ff30
tomasczerniawski <115027239+tomasczerniawski@users.noreply.github.com>
2023-02-05 13:36:10 +0200

    Update insertionSort.h

--- Lab_6/insertionSort.h
+++ Lab_6/insertionSort.h
@@ -3,45 +3,47 @@
 #include <stdio.h>
 #include <time.h>
 
-int insertion_Sort() 
-{ 
- int i = 0, j, n = 0;
-  
-    int arr[100000];
-  
-    FILE* fptr; 
-  
-    fptr = fopen("random.txt", "r"); 
-  
-    while (fscanf(fptr, "%d", &arr[i]) == 1) 
+
+int insertion_Sort()
+{
+  int i = 0, j, n = 0;
+  int arr[100000];
+
+  FILE* fptr;
+
+  fptr = fopen("random.txt", "r");
+
+  while (fscanf(fptr, "%d", &arr[i]) == 1)
+  {
+    n++;
+    i++;
+  }
+
+  clock_t t;
+  t = clock();
+
+  // logika sortowania
+  int element;
+  for (i = 1; i < n; i++)
+  {
+    element = arr[i];
+    j = i - 1;
+    while (j >= 0 && arr[j] > element)
     {
-      
-        n++; 
-  
-        i++; 
+      arr[j + 1] = arr[j];
+      j = j - 1;
     }
-    
-    clock_t t;
+    arr[j + 1] = element;
+  }
 
-    t = clock();
-     
-     //logika sortowania
-    
-    int  element; 
-    for (i = 1; i < n; i++) { element = arr[i]; j = i - 1; while (j >= 0 && arr[j] > element) { 
-            arr[j + 1] = arr[j]; 
-            j = j - 1; 
-        } 
-        arr[j + 1] = element; 
-        
-    } 
-    t = clock() - t;
-    double time_taken = ((double)t)/CLOCKS_PER_SEC;
-    
-    //printowanie tabeli
-    
-    for (i = 0; i < n; i++)
-        printf("%d \n ", arr[i]);
+  t = clock() - t;
+  double time_taken = ((double)t) / CLOCKS_PER_SEC;
 
-	printf("\n sortowanie scalieniem trwalo %f seconds \n", time_taken);
-} 
+  // printowanie tabeli
+  for (i = 0; i < n; i++)
+    printf("%d\n", arr[i]);
+
+  printf("\nSortowanie przez wstawianie trwało %f sekund\n", time_taken);
+
+  return 0;
+}
